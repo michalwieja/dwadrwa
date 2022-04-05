d5c37a5b53dd96e8b5b85230d8ef19157c25b97d
m.wieja@qarbon.pl <YqFy5aubS9DmBCG>
2022-04-05 20:21:18 +0200

    project start, font embed

---
+++ components/Header.vue
@@ -0,0 +1,78 @@
+<template>
+  <header :class="{'active' : scrollPos > 0}" class="header">
+    <div class="container">
+      <div class="logo">
+        <NuxtLink to="/">
+<!--          <img :src="logo" alt="logo">-->
+          LOGO
+        </NuxtLink>
+      </div>
+      <nav class="nav">
+        <ul class="nav__links">
+          <li v-for="link in menuLinks" :key="link.name">
+            <NuxtLink :to="link.path" class="nav__link">
+              {{ link.name }}
+            </NuxtLink>
+          </li>
+          <SocialIcons />
+        </ul>
+        <div class="burger">
+          <div class="burger__line" />
+          <div class="burger__line" />
+          <div class="burger__line" />
+        </div>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<script>
+
+import menuLinks from '../config/menu.js'
+import SocialIcons from './SocialIcons.vue'
+
+export default {
+  name: 'Header',
+  components: { SocialIcons },
+  props: {
+    transparent: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data () {
+    return {
+      menuLinks,
+      scrollPos: 0
+    }
+  },
+
+  destroyed () {
+    window.removeEventListener('scroll', this.handleScroll)
+  },
+
+  mounted () {
+    window.addEventListener('scroll', this.handleScroll)
+
+    const burger = document.querySelector('.burger')
+    const nav = document.querySelector('.nav__links')
+    const links = document.querySelectorAll('.nav__link')
+
+    burger.addEventListener('click', () => {
+      nav.classList.toggle('nav-active')
+      burger.classList.toggle('toggle')
+    })
+    links.forEach((link, index) => {
+      link.addEventListener('click', () => {
+        nav.classList.remove('nav-active')
+        burger.classList.toggle('toggle')
+      })
+    })
+  },
+  methods: {
+    handleScroll () {
+      this.scrollPos = window.scrollY
+    }
+  }
+}
+</script>
---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="app">
+    <Header/>
+    <Nuxt/>
+  </div>
+
+</template>
+
+<script>
+import Header from '../components/Header.vue';
+export default {
+  name: 'default',
+  components: { Header }
+};
+</script>
