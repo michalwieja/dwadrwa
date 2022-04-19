673afe2eb758e2978aa60ae1870a2469f4ac9fee
m.wieja@qarbon.pl <YqFy5aubS9DmBCG>
2022-04-19 18:07:09 +0200

    social icons

---
+++ pages/index.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="container">
+    <div class="socials container">
+      <a href="https://www.instagram.com/dwadrwa/" target="_blank">
+        <IgIcon style="height: 30; width: 30"/>
+      </a>
+      <a href="https://www.facebook.com/dwadrwa" target="_blank">
+        <FbIcon style="height: 30; width: 30"/>
+      </a>
+    </div>
+    <div class="hero">
+      <div class="title" data-aos="zoom-in" data-aos-delay="600" data-aos-duration="1200"> - z
+        miłości do drewna -
+      </div>
+
+    </div>
+    <div class="container">
+      Cześć, nazywam się Filip. Jeśli tu trafiłeś, to prawdopodobnym jest, że podzielasz moją miłość
+      do drewna i szukasz wyjątkowych mebli lub drewnianych dodatków do swojego wnętrza. Pozwól
+      zatem zaprosić się do mojego świata.
+
+      Prace rękodzielnicze i majsterkowanie pokochałem już za bajtla, budując szałasy i naprawiając
+      przyczepę kempingową podczas kaszubskich wakacji z rodzicami. Niedługo później zaciekawił mnie
+      również świat sztuki. Połączenie tych pasji zaprowadziło mnie do szkoły plastycznej, którą
+      ukończyłem z wyróżnieniem na kierunku metaloplastyki.
+      Druga część mojej duszy ciągnęła mnie w świat, dlatego swoje zawodowe życie związałem
+      z turystyką. Jako pilot-przewodnik pokazywałem swoim podopiecznym kraje bliższe i dalsze, sam
+      też poznając i ucząc się nowych miejsc i kultur. Szczególnie bliskie są mi Włochy,
+      niepowtarzalny klimat tamtejszych winnic, zamiłowanie do natury i prostoty, ale także
+      niezwykła architektura.
+      Fuzja tych fascynacji oraz …. pandemia, która niespodziewanie uderzyła w mój zawód, popchnęły
+      mnie do otwarcia własnej pracowni.
+      Realizuję w niej wszystko to, czym nasiąkałem przez lata – rustykalne formy przywiezione
+      z włoskiej tawerny, metalowe akcenty, które pamiętam z kuźni w katowickim plastyku, ale też
+      nowoczesne połączenia stali i drewna, które odpowiadają moim zamiłowaniom do loftowego stylu i
+      customowego zacięcia. Wszystko, co oferuję, jest dziełem moich rąk, ale też pomysłowości i
+      kreatywności. Zawsze fascynowały mnie nieregularne formy wydobywające się spod kowadła, ich
+      unikatowość i historia, jaką opowiadają. To one wykształciły mój styl.
+
+      Chętnie porozmawiam z Wami i poznam Wasze pomysły na meble i drewniane dodatki do domu, jakie
+      chcielibyście mieć. Jeśli urządzacie biuro - podpowiem jaki regał byłoby odpowiedni. Wierzę,
+      że połączenie Waszych inspiracji z moim doświadczeniem sprawi, że
+      w Waszych wnętrzach zagości coś naprawdę wyjątkowego.
+    </div>
+  </div>
+</template>
+
+<script>
+import FbIcon from '../components/FbIcon.vue';
+import IgIcon from '../components/IgIcon.vue';
+
+export default {
+  components: {
+    IgIcon,
+    FbIcon
+  }
+};
+</script>
